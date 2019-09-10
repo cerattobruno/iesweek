182050e86deee4cf54072840b8daf2680dff44ab
Damian Olivier <damianolivier7@gmail.com>
2019-09-10 14:21:56 -0300

    Mejoras en la navegación y los items activos

--- src/app/componentes/header/header.component.ts
+++ src/app/componentes/header/header.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {Router, ActivatedRoute} from '@angular/router';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class HeaderComponent {
+  public fragment:String;
+  constructor(private route: ActivatedRoute) {
+    this.route.fragment.subscribe((fragment: string) => {
+      this.fragment = fragment;
+      console.log(this.fragment);
+    });
+   }
 }
