69cefd3692778ecf0b2bb1229143b9fc300e1491
Damian Olivier <dolivier@keetup.com>
2019-09-27 10:02:43 -0300

    Mejoras galería

--- src/app/componentes/galeria/galeria.component.ts
+++ src/app/componentes/galeria/galeria.component.ts
@@ -30,26 +30,46 @@ public galeria17: Galeria;
   ) { }
 
   ngOnInit() {
-    this.galeria1 = new Galeria('../../../assets/fotos/foto01_p.JPG', 'periodismo1');
-    this.galeria2 = new Galeria('../../../assets/fotos/foto02_p.JPG', 'periodismo2');
-    this.galeria3 = new Galeria('../../../assets/fotos/foto03_p.JPG', 'periodismo3');
-    this.galeria4 = new Galeria('../../../assets/fotos/foto04_p.jpg', 'periodismo4');
-    this.galeria5 = new Galeria('../../../assets/fotos/foto05_s.JPG', 'software1');
-    this.galeria6 = new Galeria('../../../assets/fotos/foto06_s.jpg', 'software2');
-    this.galeria7 = new Galeria('../../../assets/fotos/foto07_s.JPG', 'software3');
-    this.galeria8 = new Galeria('../../../assets/fotos/foto08_s.JPG', 'software4');
-    this.galeria9 = new Galeria('../../../assets/fotos/foto09_d.jpg', 'diseño1');
-    this.galeria10 = new Galeria('../../../assets/fotos/foto10_d.JPG', 'diseño2');
-    this.galeria11 = new Galeria('../../../assets/fotos/foto11_d.JPG', 'diseño3');
-    this.galeria12 = new Galeria('../../../assets/fotos/foto12_d.JPG', 'diseño4');
-    this.galeria13 = new Galeria('../../../assets/fotos/foto13_d.JPG', 'diseño5');
-    this.galeria14 = new Galeria('../../../assets/fotos/foto14_i.jpg', 'indumen1');
-    this.galeria15 = new Galeria('../../../assets/fotos/foto15_i.jpg', 'indumen2');
-    this.galeria16 = new Galeria('../../../assets/fotos/foto16_i.jpg', 'indumen3');
-    this.galeria17 = new Galeria('../../../assets/fotos/foto17_i.jpg', 'indumen4');
-    this.galeria = [this.galeria1, this.galeria2, this.galeria3, this.galeria4, this.galeria5,
-      this.galeria6, this.galeria7, this.galeria8, this.galeria9, this.galeria10, this.galeria11,
-       this.galeria12, this.galeria13, this.galeria14, this.galeria15, this.galeria16, this.galeria17];
+    this.galeria1 = new Galeria('../../../assets/fotos/foto01_p.JPG', 'periodismo1', 'Periodismo Integral y Deportivo');
+    this.galeria2 = new Galeria('../../../assets/fotos/foto02_p.JPG', 'periodismo2', 'Periodismo Integral y Deportivo');
+    this.galeria3 = new Galeria('../../../assets/fotos/foto03_p.JPG', 'periodismo3', 'Periodismo Integral y Deportivo');
+    this.galeria4 = new Galeria('../../../assets/fotos/foto04_p.jpg', 'periodismo4', 'Periodismo Integral y Deportivo');
+    this.galeria5 = new Galeria('../../../assets/fotos/foto05_s.JPG', 'software1', 'Desarrollo de Software');
+    this.galeria6 = new Galeria('../../../assets/fotos/foto06_s.jpg', 'software2', 'Desarrollo de Software');
+    this.galeria7 = new Galeria('../../../assets/fotos/foto07_s.JPG', 'software3', 'Desarrollo de Software');
+    this.galeria8 = new Galeria('../../../assets/fotos/foto08_s.JPG', 'software4', 'Desarrollo de Software');
+    this.galeria9 = new Galeria('../../../assets/fotos/foto09_d.jpg', 'diseño1', 'Diseño Gráfico y Visual');
+    this.galeria10 = new Galeria('../../../assets/fotos/foto10_d.JPG', 'diseño2', 'Diseño Gráfico y Visual');
+    this.galeria11 = new Galeria('../../../assets/fotos/foto11_d.JPG', 'diseño3', 'Diseño Gráfico y Visual');
+    this.galeria12 = new Galeria('../../../assets/fotos/foto12_d.JPG', 'diseño4', 'Diseño Gráfico y Visual');
+    this.galeria13 = new Galeria('../../../assets/fotos/foto13_d.JPG', 'diseño5', 'Diseño Gráfico y Visual');
+    this.galeria14 = new Galeria('../../../assets/fotos/foto14_i.jpg', 'indumen1', 'Diseño y Producción de Indumentaria');
+    this.galeria15 = new Galeria('../../../assets/fotos/foto15_i.jpg', 'indumen2', 'Diseño y Producción de Indumentaria');
+    this.galeria16 = new Galeria('../../../assets/fotos/foto16_i.jpg', 'indumen3', 'Diseño y Producción de Indumentaria');
+    this.galeria17 = new Galeria('../../../assets/fotos/foto17_i.jpg', 'indumen4', 'Diseño y Producción de Indumentaria');
+    this.galeria = [this.galeria1, 
+                    this.galeria2, 
+                    this.galeria3, 
+                    this.galeria4, 
+                    this.galeria5,
+                    this.galeria6, 
+                    this.galeria7, 
+                    this.galeria8, 
+                    this.galeria9, 
+                    this.galeria10, 
+                    this.galeria11, 
+                    this.galeria12, 
+                    this.galeria13, 
+                    this.galeria14, 
+                    this.galeria15, 
+                    this.galeria16, 
+                    this.galeria17];
+      this.shuffle(this.galeria);
+      console.log(this.galeria);
+  }
+
+  shuffle(array) {
+    array.sort(() => Math.random() - 0.5);
   }
 
 }
